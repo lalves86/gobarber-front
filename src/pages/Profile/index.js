9a0766c68c8eb55e93b6f31c4f17f27649a13ecb
lalves86 <lucas.alves@engenharia.ufjf.br>
2020-03-16 15:17:52 -0300

    Go Barber web

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
+
+export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Sua senha" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmar senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
+}
